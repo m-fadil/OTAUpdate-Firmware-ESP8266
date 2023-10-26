5322fb903ad62d15e2df72555441cce44f033d2b
UsrFdL <fadhilaikhsani@yahoo.com>
2023-10-26 22:41:10 +0700

    initial commit

---
+++ subscriber.js
@@ -0,0 +1,31 @@
+const mqtt = require("mqtt");
+
+const protocol = 'mqtt'
+const host = "mqtt.eclipseprojects.io"
+const port = '1883'
+const topic = ["klien_cekESP",]
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+const connectUrl = `${protocol}://${host}:${port}`
+
+
+const client = mqtt.connect(connectUrl, {
+    clientId: clientId,
+    connectTimeout: 4000,
+    reconnectPeriod: 1000,
+})
+  
+client.on('connect', () => {
+    console.log('Connected')
+    client.subscribe(topic, () => {
+        console.log(`Subscribe to topic '${topic}'`)
+    })
+    
+})
+
+
+client.on('message', (topic, message) => {
+    // message is a Buffer
+    let strMessage = message.toString();
+    // let objMessage = JSON.parse(strMessage);
+    console.log(strMessage);
+})
