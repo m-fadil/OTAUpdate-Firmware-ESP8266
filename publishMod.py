3a35e46a27e146f2927e675eb17468040db7e592
UsrFdL <fadhilaikhsani@yahoo.com>
2023-10-26 17:25:39 +0700

    50%

--- publishMod.py
+++ publishMod.py
@@ -5,6 +5,7 @@ import sys
 import platform
 import time
 import threading
+import itertools
 
 def clear():
     if platform.system() == 'Windows':
@@ -16,30 +17,51 @@ class Klien():
     def __init__(self):
         self.mqtt_server = "mqtt.eclipseprojects.io"
         self.mqtt_topic = ["klien_cekESP", "klien_updateTunggal"]
+        self.payload = time.ctime()
         self.client = mqtt.Client(f"Klien-{random.randint(1, 999)}")
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
         self.client.on_unsubscribe = self.on_unsubscribe
+
+        self.done = False
         self.onESP = []
 
+        self.pesan = ""
+        self.updated = False
+
     def on_connect(self, client, userdata, flags, rc):
-        print("Terhubung dengan server MQTT")
+        print("\rTerhubung dengan server MQTT")
+        self.menu()
         
     def on_message(self, client, userdata, msg):
-        pesan = str(msg.payload.decode('utf-8'))
-        if msg.topic == "klien_cekESP":
+        self.pesan = str(msg.payload.decode('utf-8'))
+        if msg.topic == self.mqtt_topic[0]:
             print(".", end=" ")
             sys.stdout.flush()
-            self.onESP.append(pesan)
+            self.onESP.append(self.pesan)
+        elif msg.topic == self.mqtt_topic[1]:
+            self.result()
 
     def on_unsubscribe(self, client, userdata, mid):
         pass
 
-    def set_timeout(sefl, func, sec):
-        t = threading.Timer(sec, func)
-        t.start()
+    def set_loading(self, func=None, sec=None):
+        self.done = False
+        ti = threading.Timer(sec, func)
+        ti.start()
+        th = threading.Thread(target=self.animate)
+        th.start()
+
+    def animate(self):
+        for c in itertools.cycle(["⢿ ", "⣻ ", "⣽ ", "⣾ ", "⣷ ", "⣯ ", "⣟ ", "⡿ "]):
+            if self.done:
+                break
+            sys.stdout.write('\rloading ' + c)
+            sys.stdout.flush()
+            time.sleep(0.1)
 
     def updateTunggal(self):
+        self.done = True
         clear()
         for i in range(len(self.onESP)):
             print(f"{i+1}. {self.onESP[i]}")
@@ -48,7 +70,7 @@ class Klien():
         try:
             pilih = int(pilih)
         except:
-            print("Masukan harus Angka atau q untuk keluar")
+            print("Masukan harus Angka")
             time.sleep(2)
             self.updateTunggal()
         else:
@@ -56,42 +78,48 @@ class Klien():
                 self.menu()
             else:
                 topic = self.onESP[pilih - 1]
-                payload = "update"
                 self.client.subscribe(self.mqtt_topic[1])
-                self.client.publish(topic, payload)
+                self.client.publish(topic, self.payload)
+                self.set_loading(self.result, 8)
                 clear()
-                print("memuat ESP")
-                self.set_timeout(self.menu, 3)
+
+    def result(self):
+        self.done = True
+        if self.updated:
+            print(f"Proses update firmware pada {self.pesan} berhasil")
+        else:
+            print("gagal")
+        input("Tekan ENTER untuk melanjutkan")
+        self.menu()
 
     def cekESP(self, route):
+        self.done = True
         topic = "cekESP"
-        payload = "cek"
         self.onESP.clear()
         self.client.subscribe(self.mqtt_topic[0])
-        self.client.publish(topic, payload)
+        self.client.publish(topic, self.payload)
         if route == 1:
-            self.set_timeout(self.updateTunggal, 3)
+            self.set_loading(self.updateTunggal, 3)
         elif route == 9:
             self.menu()
         elif route == 0:
             sys.exit()
         else:
             self.menu()
-        clear()
-        print("memuat ESP")
 
     def menu(self):
         clear()
+        # self.done = True
         self.client.unsubscribe(self.mqtt_topic)
         print("1. Update per-satu ESP\n9. Kembali\n0. Keluar")
         pilih = int(input("-> "))
         self.cekESP(pilih)
         
     def run(self):
+        print("\rMembangun koneksi dengan MQTT ...")
         self.client.connect(self.mqtt_server, 1883, 60)
-        print("Membangun koneksi dengan MQTT ...")
-        self.client.loop_start()
-        self.set_timeout(self.menu, 3)
+        # self.set_loading(self.menu, 3)
+        self.client.loop_forever()
 
 if __name__ == "__main__":
     klien = Klien()
